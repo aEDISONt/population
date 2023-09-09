da85827b29e0a7a163bc30d07b47af8935c1cd7c
Antony Edison <143955053+aEDISONt@users.noreply.github.com>
2023-09-09 14:22:48 +0000

    Sat, Sep 9, 2023, 7:52 PM +05:30

--- population/lab1.c
+++ population/lab1.c
@@ -1,31 +1,31 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 int main(void)
 {
-  int ip,fp;
-  do
-  {
-     ip = get_int("enter intial population : ");
-  }
-  while(ip<9);
-  // final population needed
-  do
-  {
-    fp = get_int("enter final population : ");
-  }
-  while(fp<ip);
+    int ip, fp;
+    do
+    {
+        ip = get_int("enter intial population : ");
+    }
+    while (ip < 9);
+    // final population needed
+    do
+    {
+        fp = get_int("enter final population : ");
+    }
+    while (fp < ip);
 
-  int inp;
-  int years = 0;
-  //calculation of yrs needed for final population
-  while(ip<fp)
-  {
-   int born = ip/3;
-   int dead = ip/4;
-   inp = born - dead;
-   ip += inp;
-   years++;
+    int inp;
+    int years = 0;
+    // calculation of yrs needed for final population
+    while (ip < fp)
+    {
+        int born = ip / 3;
+        int dead = ip / 4;
+        inp = born - dead;
+        ip += inp;
+        years++;
 
-  }
+    }
   printf("years : %i \n" ,years);
 }
