ad4692cdedf7620e806933d7691055e8a7ad51b1
Antony Edison <143955053+aEDISONt@users.noreply.github.com>
2023-09-09 13:43:30 +0000

    Sat, Sep 9, 2023, 7:13 PM +05:30

--- population/second.c
+++ population/second.c
@@ -8,4 +8,17 @@ int main(void)
     int ip = get_int();
   }
   while(ip<9);
+  // final population needed
+  do
+  {
+    printf("enter final population : ");
+    int fp = get_int();
+  }
+  while(fp>ip);
+
+  //calculation of yrs needed for final population
+  while(ip<fp)
+  {
+    
+  }
 }
